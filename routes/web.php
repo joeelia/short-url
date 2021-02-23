eecea19866571f6a16b24339c9bd55184f8a0730
Joe Elia <44442621+joeelia@users.noreply.github.com>
2021-02-22 22:05:15 -0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('/short/{shortURLKey}', 'AshAllenDesign\ShortURL\Controllers\ShortURLController')->name('short-url.invoke');
+// Route::get('/'.config('short-url.short').'{shortURLKey}', 'joeeliarollers\ShortURLController')->name('short-url.invoke');
